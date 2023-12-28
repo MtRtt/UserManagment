dfc2718369c4e2fb1e2d9d65913dfb204ac8baf6
mohrt <mohrt@MOH_RTT>
2023-12-28 12:51:23 +0330

    Add project files.

---
+++ UserManagment/Controllers/CategoryController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserManagment.Data;
+using UserManagment.Models;
+
+namespace UserManagment.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly UserDbContext _UserDB;
+        public CategoryController(UserDbContext userDb)
+        {
+            _UserDB = userDb;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> CategoryList = _UserDB.categories;
+            return View(CategoryList);
+        }
+
+        //Get
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _UserDB.categories.Add(obj);
+                _UserDB.SaveChanges();
+                //return View();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //Get for Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _UserDB.categories.Find(id);
+            // var categoryFromDbFirst = _UserDB.categories.FirstOrDefault(u => u.Id == id);
+            //var categoryFromSiingle = _UserDB.categories.SingleOrDefault(u => u.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //Post for Edit
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _UserDB.categories.Update(obj);
+                _UserDB.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //Get for Edit
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _UserDB.categories.Find(id);
+            // var categoryFromDbFirst = _UserDB.categories.FirstOrDefault(u => u.Id == id);
+            //var categoryFromSiingle = _UserDB.categories.SingleOrDefault(u => u.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //Post for Edit
+        [HttpPost]
+        public IActionResult DeletePost(int? id)
+        {
+
+            var obj = _UserDB.categories.Find(id);
+            _UserDB.categories.Remove(obj);
+            _UserDB.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+    }
+
+}
---
+++ UserManagment/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserManagment.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage="عنوان دسته اجباری می باشد")]
+        [DisplayName("عنوان دسته")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "ترتیب نمایش اجباری می باشد")]
+        [DisplayName("ترتیب نمایش")]
+        [Range(1,100, ErrorMessage="از یک تا 100 قابل قبول است")]
+        public int Displayoreder { get; set; }
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+    }
+}
